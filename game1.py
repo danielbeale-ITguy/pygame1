07c6f8e8e96707a861463928d3479ef4dd6c68f6
danielbeale-ITguy <danielbeale719@gmail.com>
2025-03-03 18:48:27 +1100

    Update game1.py

--- game1.py
+++ game1.py
@@ -9,63 +9,64 @@ GREEN = (0, 255, 0)
 
 
 pygame.init()
-
 WIDTH = 640
-HEIGHT = 640
-
-
+HEIGHT = 320
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-
-
 clock = pygame.time.Clock()
 
-img = pygame.image.load('ground.png')
-img.convert()
 
-rect_1  = img.get_rect()
+img1 = pygame.image.load('images/path1.png')
+
+
+rect_2 = img1.get_rect()
+
+
 
 
 
 tile_size= 64
-
 game_map = [[0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0],
-            [0,0,1,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,1,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,1,1,0,0,1,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0]]
-
+            [1,1,1,1,1,1,1,1,1,1]]        
 tile_list = []
 
 
-running = True
 
+
+
+
+playercharacter = pygame.image.load('images/cube1.png')
+playercharacterrect = playercharacter.get_rect()
+
+
+playerright = False
+playerleft = False
+playerup = False
+playerdown = False
+
+
+
+
+running = True
 
 while running:  
     clock.tick(60)
     screen.fill(GRAY)
 
+    pygame.draw.rect(screen,RED,playercharacterrect)
 
     
     tile_list = []
-
     y = 0
     for row in game_map:
         x = 0
         for segment in row:
             if segment == 1:
-              screen.blit(img,(x*64,y*64))
+              screen.blit(img1,(x*64,y*64))
             x += 1
         y += 1
-
-
-
-
-
 
 
 
@@ -76,17 +77,41 @@ while running:
 
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
-                rect_1.right += 1
-            
+                playerright  = True
+
+
+
+
+
+
+
+
+
+
+
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                playerright = False
+
+
+
+
+    if playerright == True:
+        playercharacterrect.right += 1
+
+
 
 
 
 
     
-
-    
+    screen.blit(playercharacter,(playercharacterrect))
     pygame.display.flip()
     pygame.display.update()
 
 
 pygame.quit()
+
+
+
+  
