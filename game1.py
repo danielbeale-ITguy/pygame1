c220e4fbcca9e4cf7a3ad8d8af373e513b233838
danielbeale-ITguy <danielbeale719@gmail.com>
2025-03-05 21:36:56 +1100

    Update game1.py

--- game1.py
+++ game1.py
@@ -13,52 +13,70 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
 clock = pygame.time.Clock()
 
 
+x = 0
+y = 0
 
-snake = pygame.rect.Rect([100,100,TILE-2,TILE-2])
-segments = [snake.copy()]
-length = 1
-snake_dir = (0,0)
+
+snake_body = [(x,y)]
+
+  
+
+Direction = 'LEFT'
+
 
 
 running = True
 while running:  
 
-    clock.tick(60)
+    clock.tick(10)
 
+    
+    
     screen.fill('black')
-    
-    for segment in segments:
-        pygame.draw.rect(screen,'green',segment)
-    
 
+    
+    fruit = pygame.draw.rect(screen,'blue',(100,0,TILE,TILE))
+    
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
- 
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
-                snake_dir = (0,-TILE)
-            if event.key == K_DOWN:
-                snake_dir = (0,TILE)
-            if event.key == K_LEFT:
-                snake_dir = (-TILE,0)
+        if event.type ==KEYDOWN:
             if event.key == K_RIGHT:
-                snake_dir = (TILE,0)    
+                Direction = 'RIGHT'        
+            
+
+
 
     
+    if Direction == 'RIGHT':
+        x += TILE
 
-  
 
-    snake.move_ip(snake_dir)
-    segments.append(snake.copy())
-    segments = segments[-length:]
+    snake_body.insert(0,(x,y))
+
+    #find way to check if snake body postion 1 == Fruit Rect if so increase then snake body insert again
+#    if Direction == 'RIGHT':
+#        snake_body.insert(0,(x,y))    
+    
+
+
+
+
+    snake_body.pop()
+
+#   
+ 
+    for i in snake_body:
+        pygame.draw.rect(screen,'pink',(i[0],i[1],TILE,TILE))
+
+ 
+    
+        
     
     pygame.display.flip()
     pygame.display.update()
 
 
 pygame.quit()
-
-  
